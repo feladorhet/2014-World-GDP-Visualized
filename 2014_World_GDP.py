8ec4ae310cde631da1f3b8cc0da60c914fb43de8
Meriç Buğra Haliloğlu <92290449+feladorhet@users.noreply.github.com>
2023-02-18 14:48:50 +0300

    Add files via upload

---
+++ 2014_World_GDP.py
@@ -0,0 +1,23 @@
+import chart_studio.plotly as py
+import plotly.graph_objects as go
+from plotly.offline import download_plotlyjs, plot, iplot
+import pandas as pd
+
+df = pd.read_csv("2014_World_GDP.csv")
+print(df.columns)
+
+#the data for choropleth
+mydata = dict(type="choropleth",
+            locations=df["CODE"],
+            locationmode = "ISO-3",
+            z = df["GDP (BILLIONS)"],
+            text = df["COUNTRY"],
+            colorbar = {"title":"GDP IN BILLIONS"},
+            marker = dict(line = dict(color = "rgb(255,255,255)", width = 1))
+            )
+mylayout = dict(title="2014 World GDP",
+            geo = dict(scope="world", showlakes=True, lakecolor = "rgb(85,173,240)", showframe=False)
+            )
+
+myChoroMap = go.Figure(data=[mydata], layout=mylayout)
+iplot(myChoroMap)
